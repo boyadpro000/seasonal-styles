6f677348412b3d7eef2e8b47c11d0e437782ad59
Prodan.Boyadzhiev@seattlecolleges.edu <Prodan.Boyadzhiev@seattlecolleges.edu>
2022-07-27 23:15:07 -0700

    finished

--- js/seasonal-styles.js
+++ js/seasonal-styles.js
@@ -3,5 +3,67 @@ $("document").ready(function(){
         e.preventDefault();//stop default submission
         var season = $(this).attr("href");//contents of href attribute of this element
         alert(season);
+
+        switch(season){
+
+        case "Spring":{
+        $("html").css("background-color","#2B7129");
+
+        $("#wear").attr("src","images/spring-wear.jpg");
+
+        $("#logo").attr("src","images/spring.gif");
+
+        $("header h3").text("Jump Into Spring Wear!");
+        }
+        break;
+
+        
+
+            case "Summer":{
+            $("html").css("background-color","#EBA52B");
+    
+            $("#wear").attr("src","images/summer-wear.jpg");
+    
+            $("#logo").attr("src","images/summer.gif");
+    
+            $("header h3").text("Jump Into Summer Wear!");
+            }
+            break;
+
+                case "Fall":{
+                $("html").css("background-color","#A81124");
+        
+                $("#wear").attr("src","images/fall-wear.jpg");
+        
+                $("#logo").attr("src","images/fall.gif");
+        
+                $("header h3").text("Jump Into Fall Wear!");
+                }
+                break;
+
+                    case "Winter":{
+                    $("html").css("background-color","#005393");
+            
+                    $("#wear").attr("src","images/winter-wear.jpg");
+            
+                    $("#logo").attr("src","images/winter.gif");
+            
+                    $("header h3").text("Jump Into Winter Wear!");
+                    }
+                    break;
+        
+                    case "Default":{
+                        $("html").css("background-color","gray");
+                
+                        $("#wear").attr("src","images/hanger.jpg");
+                
+                        $("#logo").attr("src","images/four-seasons.gif");
+                
+                        $("header h3").text("Outfitter for All Seasons!");
+                        }
+                        break;
+            
+    
+        }
     });
 });
